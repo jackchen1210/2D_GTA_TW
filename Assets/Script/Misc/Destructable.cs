82547197b43ff9b6637447255322231977f4c0a9
JackChen <jack3492843@gmail.com>
2024-01-26 19:10:45 +0800

    新增效果

--- Assets/Script/Misc/Destructable.cs
+++ Assets/Script/Misc/Destructable.cs
@@ -1,9 +1,14 @@
 using System;
 using UnityEngine;
+using DG.Tweening;
 
 public class Destructable : MonoBehaviour
 {
     [SerializeField] private GameObject destructVFX;
+    [SerializeField] private int destructTime = 1;
+
+    private Tween shakeTween;
+    private int currentDestructCounter;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,7 +20,24 @@ public class Destructable : MonoBehaviour
 
     private void Destruct()
     {
+        currentDestructCounter++;
+        if (currentDestructCounter < destructTime)
+        {
+            Shake();
+            return;
+        }
         Instantiate(destructVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    private void Shake()
+    {
+        shakeTween?.Kill();
+        shakeTween = transform.DOShakePosition(0.1f).SetRelative();
+    }
+
+    private void OnDestroy()
+    {
+        shakeTween?.Kill();
     }
 }
