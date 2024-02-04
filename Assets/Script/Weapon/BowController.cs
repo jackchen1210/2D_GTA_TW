88f4d6569b8e3a5628d36813d57e7d60cacce820
JackChen <jack3492843@gmail.com>
2024-02-04 09:19:37 +0800

    新增武器攻擊效果

--- Assets/Script/Weapon/BowController.cs
+++ Assets/Script/Weapon/BowController.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class BowController : MonoBehaviour, IWeapon
 {
+    [SerializeField] private Animator animator;
+    private int shootAniHash;
+
+    private void Awake()
+    {
+        shootAniHash = Animator.StringToHash("Shoot");
+    }
     public void Attack()
     {
         Debug.Log("Bow Attack");
+        animator.SetTrigger(shootAniHash);
     }
 
     public GameObject GetGameObject()
--- Assets/Script/Weapon/StaffController.cs
+++ Assets/Script/Weapon/StaffController.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class StaffController : MonoBehaviour, IWeapon
 {
+    [SerializeField] private Animator animator;
+    private int swingAniHash;
+
+    private void Awake()
+    {
+        swingAniHash = Animator.StringToHash("StaffSwing");
+    }
+
     public void Attack()
     {
         Debug.Log("Staff Attack");
+        animator.SetTrigger(swingAniHash);
     }
 
     public GameObject GetGameObject()
